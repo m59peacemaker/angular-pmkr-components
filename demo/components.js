38e9ec6a85336ecf4a323dbc78efce416ac59cbe
Johnny Hauser <johnnyhauser@gmail.com>
2014-09-11 02:29:59 -0500

    tested filterStabilize

--- demo/components.js
+++ demo/components.js
@@ -9,13 +9,6 @@ angular.module('pmkr.components.directives', [
   'pmkr.validateCustom'
 ]);
 
-angular.module('pmkr.components.services', [
-  'pmkr.debounce',
-  'pmkr.memoize',
-  'pmkr.filterStabilize',
-  'pmkr.rethrowException'
-]);
-
 angular.module('pmkr.components.filters', [
   'pmkr.offset',
   'pmkr.partition',
@@ -24,6 +17,13 @@ angular.module('pmkr.components.filters', [
   'pmkr.stripTags',
   'pmkr.spaceSentences',
   'pmkr.limitEllipsis'
+]);
+
+angular.module('pmkr.components.services', [
+  'pmkr.debounce',
+  'pmkr.memoize',
+  'pmkr.filterStabilize',
+  'pmkr.rethrowException'
 ]);
 
 angular.module('pmkr.pristineOriginal', [])
@@ -161,6 +161,221 @@ angular.module('pmkr.validateCustom', [
 
   }
 ])
+
+;
+
+angular.module('pmkr.limitEllipsis', [
+  'pmkr.stripTags',
+  'pmkr.spaceSentences'
+])
+
+.filter('pmkr.limitEllipsis', [
+  '$filter',
+  function($filter) {
+
+    var stripTags = $filter('pmkr.stripTags');
+    var spaceSentences = $filter('pmkr.spaceSentences');
+    var limitTo = $filter('limitTo');
+
+    function filter(str, limit, ellipsis) {
+
+      if (!str || !str.length) { return str; }
+
+      ellipsis = ellipsis || '...';
+
+      var text = spaceSentences(stripTags(str));
+
+      var limited = limitTo(text, limit);
+
+      if (limited === text) {
+        return limited;
+      }
+
+      return limited+ellipsis;
+
+    }
+
+    return filter;
+
+  }
+])
+
+;
+
+angular.module('pmkr.offset', [])
+
+.filter('pmkr.offset', [
+  function() {
+
+    function filter(input, offset) {
+
+      if (!input || !input.length) { return input; }
+
+      offset = parseInt(offset, 10);
+      return input.slice(offset);
+
+    }
+
+    return filter;
+
+  }
+])
+
+;
+
+angular.module('pmkr.partition', [
+  'pmkr.filterStabilize'
+])
+
+.filter('pmkr.partition', [
+  'pmkr.filterStabilize',
+  function(stabilize) {
+
+    var filter = stabilize(function(input, size) {
+
+      if (!input || !size) {
+        return input;
+      }
+
+      var newArr = [];
+
+      for (var i = 0; i < input.length; i+= size) {
+        newArr.push(input.slice(i, i+size));
+      }
+
+      return newArr;
+
+    });
+
+    return filter;
+
+  }
+])
+
+;
+
+angular.module('pmkr.slugify', [])
+
+.filter('pmkr.slugify', [
+  function() {
+
+    function filter(str) {
+
+      if (!str) { return str; }
+
+      var slug = str
+        .toLowerCase()
+        .replace(/ /g, '-')
+        .replace(/[^\w-]+/g, '')
+      ;
+
+      return slug;
+
+    }
+
+    return filter;
+
+  }
+])
+
+;
+
+angular.module('pmkr.shuffle', [
+  'pmkr.filterStabilize'
+])
+
+.filter('pmkr.shuffle', [
+  'pmkr.filterStabilize',
+  function(stabilize) {
+
+    var filter = stabilize(function(input) {
+
+      if (!input) { return input; }
+
+      if (typeof input === 'string') {
+        input = input.split('');
+        shuffle(input);
+        return input.join('');
+      } else {
+        shuffle(input);
+      }
+
+    });
+
+    // Fisher-Yates shuffle (https:github.com/coolaj86/knuth-shuffle)
+    function shuffle(arr) {
+
+      var currentIndex = arr.length;
+
+      while (0 !== currentIndex) {
+
+        var randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex-= 1;
+
+        var temporaryValue = arr[currentIndex];
+        arr[currentIndex] = arr[randomIndex];
+        arr[randomIndex] = temporaryValue;
+      }
+
+      return arr;
+
+    }
+
+    return filter;
+
+  }
+])
+
+;
+
+angular.module('pmkr.spaceSentences', [])
+
+.filter('pmkr.spaceSentences', [
+  function() {
+
+    function filter(str) {
+
+      if (!str) { return str; }
+
+      var spaced = str.replace(/(\w)([.!?]+)(\w)/gi, '$1$2 $3');
+
+      return spaced;
+
+    }
+
+    return filter;
+
+  }
+])
+
+;
+
+angular.module('pmkr.stripTags', [])
+
+.filter('pmkr.stripTags', function () {
+
+  function filter(str, tags, disallow)  {
+
+    if (!str) { return str; }
+
+    tags = tags ? tags.split(',') : '';
+
+    var regexp = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi;
+
+    var stripped = str.replace(regexp, function($0, $1) {
+      var found = ~tags.indexOf($1.toLowerCase());
+      var replace = disallow ? found : !found;
+      var replacement = !replace ? $0 : '';
+      return replacement;
+    });
+
+    return stripped;
+
+  }
+
+  return filter;
+
+})
 
 ;
 
@@ -302,37 +517,6 @@ angular.module('pmkr.filterStabilize', [
 
 ;
 
-angular.module('pmkr.rethrowException', [])
-
-.provider('pmkr.rethrowException', [
-  '$provide',
-  function($provide) {
-
-    this.init = function() {
-      $provide.decorator('$exceptionHandler', [
-        '$delegate',
-        decorator
-      ]);
-    };
-
-    function decorator($delegate) {
-
-      function decorated(exception, cause) {
-        $delegate(exception, cause);
-        throw exception;
-      }
-
-      return decorated;
-
-    }
-
-    this.$get = function() {};
-
-  }
-])
-
-;
-
 angular.module('pmkr.memoize', [])
 
 .factory('pmkr.memoize', [
@@ -374,217 +558,33 @@ angular.module('pmkr.memoize', [])
 
 ;
 
-angular.module('pmkr.limitEllipsis', [
-  'pmkr.stripTags',
-  'pmkr.spaceSentences'
-])
+angular.module('pmkr.rethrowException', [])
 
-.filter('pmkr.limitEllipsis', [
-  '$filter',
-  function($filter) {
+.provider('pmkr.rethrowException', [
+  '$provide',
+  function($provide) {
 
-    var stripTags = $filter('pmkr.stripTags');
-    var spaceSentences = $filter('pmkr.spaceSentences');
-    var limitTo = $filter('limitTo');
+    this.init = function() {
+      $provide.decorator('$exceptionHandler', [
+        '$delegate',
+        decorator
+      ]);
+    };
 
-    function filter(str, limit, ellipsis) {
+    function decorator($delegate) {
 
-      if (!str || !str.length) { return str; }
-
-      ellipsis = ellipsis || '...';
-
-      var text = spaceSentences(stripTags(str));
-
-      var limited = limitTo(text, limit);
-
-      if (limited === text) {
-        return limited;
+      function decorated(exception, cause) {
+        $delegate(exception, cause);
+        throw exception;
       }
 
-      return limited+ellipsis;
+      return decorated;
 
     }
 
-    return filter;
+    this.$get = function() {};
 
   }
 ])
-
-;
-
-angular.module('pmkr.offset', [])
-
-.filter('pmkr.offset', [
-  function() {
-
-    function filter(input, offset) {
-
-      if (!input || !input.length) { return input; }
-
-      offset = parseInt(offset, 10);
-      return input.slice(offset);
-
-    }
-
-    return filter;
-
-  }
-])
-
-;
-
-angular.module('pmkr.partition', [
-  'pmkr.filterStabilize'
-])
-
-.filter('pmkr.partition', [
-  'pmkr.filterStabilize',
-  function(stabilize) {
-
-    var filter = stabilize(function(input, size) {
-
-      if (!input || !size) {
-        return input;
-      }
-
-      var newArr = [];
-
-      for (var i = 0; i < input.length; i+= size) {
-        newArr.push(input.slice(i, i+size));
-      }
-
-      return newArr;
-
-    });
-
-    return filter;
-
-  }
-])
-
-;
-
-angular.module('pmkr.shuffle', [
-  'pmkr.filterStabilize'
-])
-
-.filter('pmkr.shuffle', [
-  'pmkr.filterStabilize',
-  function(stabilize) {
-
-    var filter = stabilize(function(input) {
-
-      if (!input) { return input; }
-
-      if (typeof input === 'string') {
-        input = input.split('');
-        shuffle(input);
-        return input.join('');
-      } else {
-        shuffle(input);
-      }
-
-    });
-
-    // Fisher-Yates shuffle (https:github.com/coolaj86/knuth-shuffle)
-    function shuffle(arr) {
-
-      var currentIndex = arr.length;
-
-      while (0 !== currentIndex) {
-
-        var randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex-= 1;
-
-        var temporaryValue = arr[currentIndex];
-        arr[currentIndex] = arr[randomIndex];
-        arr[randomIndex] = temporaryValue;
-      }
-
-      return arr;
-
-    }
-
-    return filter;
-
-  }
-])
-
-;
-
-angular.module('pmkr.slugify', [])
-
-.filter('pmkr.slugify', [
-  function() {
-
-    function filter(str) {
-
-      if (!str) { return str; }
-
-      var slug = str
-        .toLowerCase()
-        .replace(/ /g, '-')
-        .replace(/[^\w-]+/g, '')
-      ;
-
-      return slug;
-
-    }
-
-    return filter;
-
-  }
-])
-
-;
-
-angular.module('pmkr.spaceSentences', [])
-
-.filter('pmkr.spaceSentences', [
-  function() {
-
-    function filter(str) {
-
-      if (!str) { return str; }
-
-      var spaced = str.replace(/(\w)([.!?]+)(\w)/gi, '$1$2 $3');
-
-      return spaced;
-
-    }
-
-    return filter;
-
-  }
-])
-
-;
-
-angular.module('pmkr.stripTags', [])
-
-.filter('pmkr.stripTags', function () {
-
-  function filter(str, tags, disallow)  {
-
-    if (!str) { return str; }
-
-    tags = tags ? tags.split(',') : '';
-
-    var regexp = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi;
-
-    var stripped = str.replace(regexp, function($0, $1) {
-      var found = ~tags.indexOf($1.toLowerCase());
-      var replace = disallow ? found : !found;
-      var replacement = !replace ? $0 : '';
-      return replacement;
-    });
-
-    return stripped;
-
-  }
-
-  return filter;
-
-})
 
 ;
---
+++ src/services/filterStabilize/tests/filterStabilize.spec.js
@@ -0,0 +1,49 @@
+describe('filterStabilize', function() {
+
+  var $filterProvider;
+  var $compile;
+  var $scope;
+
+  beforeEach(module('pmkr.filterStabilize'));
+
+  beforeEach(module(function(_$filterProvider_) {
+    $filterProvider = _$filterProvider_;
+  }));
+
+  beforeEach(inject(function(_$rootScope_, _$compile_) {
+    $compile = _$compile_;
+    $scope = _$rootScope_;
+  }));
+
+  it('is needed to prevent infinite digest when filter is unstable', function() {
+    $filterProvider.register('testFilter', function() {
+      var i = 0;
+      return function() { return ++i; };
+    });
+    var elem = angular.element("<p>{{'abc'|testFilter}}</p>");
+    $compile(elem)($scope);
+    expect($scope.$digest.bind($scope)).toThrow();
+  });
+
+  it('stabilizes unstable filter', function() {
+    $filterProvider.register('testFilter', ['pmkr.filterStabilize', function(stabilize) {
+      var i = 0;
+      return stabilize(function() { return ++i; });
+    }]);
+    var elem = angular.element("<p>{{'abc'|testFilter}}</p>");
+    $compile(elem)($scope);
+    expect($scope.$digest.bind($scope)).not.toThrow();
+  });
+
+  it('prevents filter from modifying input reference', function() {
+    $filterProvider.register('testFilter', ['pmkr.filterStabilize', function(stabilize) {
+      return stabilize(function(arr) { return arr.pop(); });
+    }]);
+    $scope.foo = [1,2,3];
+    var elem = angular.element("<p>{{foo|testFilter}}</p>");
+    $compile(elem)($scope);
+    $scope.$digest();
+    expect($scope.foo).toEqual([1,2,3]);
+  });
+
+});
