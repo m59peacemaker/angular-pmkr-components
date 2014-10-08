758b72812a23201e5f10eefb2a4a430f8174e9be
Richard Littauer <richard.littauer@gmail.com>
2014-10-08 15:28:54 -0700

    Added an option for the amount of partitions
    This may be helpful. Was for me.

--- src/filters/partition/partition.js
+++ src/filters/partition/partition.js
@@ -6,15 +6,17 @@ angular.module('pmkr.partition', [
   'pmkr.filterStabilize',
   function(stabilize) {
 
-    var filter = stabilize(function(input, size) {
+    var filter = stabilize(function(input, size, partitionAmount) {
 
       if (!input || !size) {
         return input;
       }
 
+      partitionAmount = (partitionAmount === undefined) ? input.length : partitionAmount*size;
+
       var newArr = [];
 
-      for (var i = 0; i < input.length; i+= size) {
+      for (var i = 0; i < partitionAmount; i+= size) {
         newArr.push(input.slice(i, i+size));
       }
 
